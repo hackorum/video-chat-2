6eee586a9db94ba6b2c36901534a6a0e3431bbaf
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2022-08-28 17:12:24 +0530

    Eleventh Commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
+console.log("helloo");
 
 const { v4: uuidv4 } = require("uuid");
 
